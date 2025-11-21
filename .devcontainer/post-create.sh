33e0c9844fce07ab7c7488d4adf985a592a9b1d9
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-11-21 15:36:15 +0100

    Add README, LICENSE and LLVM-Wrapper

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-apt update
-apt install -y gdb
+# Configure CMake project
 rm -rf build
 cmake -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+# Install gdb
+apt update
+apt install -y gdb
