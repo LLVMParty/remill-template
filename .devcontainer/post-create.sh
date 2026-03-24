678f11f7adc27788049601def9f6037c8646bcb6
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2026-03-24 12:38:10 +0100

    Fix devcontainer startup errors about kitware repo signing key

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Configure CMake project
 rm -rf build
-cmake -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
+
+# Remove kitware.list (signature rotation is problematic)
+sudo rm -f /etc/apt/sources.list.d/kitware.list
 
 # Install gdb
 apt update
